65da560521feda4cf6e94320a7f7a3824dc35426
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-02-10 16:40:42 +0900

    [level 1] Title: 과일 장수, Time: 0.10 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.swift
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.swift
@@ -1,10 +1,11 @@
+import Foundation
+
 func solution(_ k:Int, _ m:Int, _ score:[Int]) -> Int {
     var answer = 0
     var score = score.sorted{$0 > $1}
     var start = m-1
-    while start < score.count {        
-        answer += m*score[start] // 정렬을 하고 해당번째로 끊기고 끊기는 수가 가장 작은 수 
-        start += m
+    for i in stride(from: start, to: score.count, by: m) {
+        answer += score[i] * m
     }
 
     return answer
