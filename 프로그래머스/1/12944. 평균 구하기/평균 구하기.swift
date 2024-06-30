f68ad58136cc0eaa11465762a17a36c109a1562f
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-06-30 13:27:56 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.swift
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.swift
@@ -1,5 +1,4 @@
 func solution(_ arr:[Int]) -> Double {
-    
-    let total = Double(arr.reduce(0) { $0 + $1 }) / Double(arr.count)
-    return total
+    let result = Double(arr.reduce(0) { $0 + $1}) / Double(arr.count)
+    return result
 }
