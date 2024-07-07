241042ee62730cdbe2075a3f033442eb1662064a
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-07-07 15:26:45 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.21 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
@@ -1,20 +1,10 @@
 func solution(_ arr:[Int]) -> [Int] {
     
-    guard arr.count != 1 else { return [-1] }
+    guard arr.count > 1 else { return [-1] }
     
-    var initialArr = arr
-    var previousValue : Int = arr.reduce(0) { $0 + $1 }
-    var minIndex = 0
-    for (index, element) in arr.enumerated() {
-        
-        
-        if element < previousValue {
-            previousValue = element
-            minIndex = index
-        }
-    }
+    var min = arr.min()!
     
-    initialArr.remove(at: minIndex)
+    let result = arr.filter { $0 != min }
     
-   return initialArr
+    return result
 }
