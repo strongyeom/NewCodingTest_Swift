259a7c588cb3d009d887caaad98b311588df6eef
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-09-07 16:45:50 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 9439.71 ms, Memory: 306 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.swift
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.swift
@@ -1,12 +1,11 @@
 func solution(_ a:Int, _ b:Int) -> Int64 {
     
-   let minValue = a < b ? a : b
-    let maxValue = a > b ? a : b
-
-    var value: Int64 = 0
-    for i in minValue...maxValue {
-        value += Int64(i)
-    }
-
-    return value
+    // 1. 범위 설정해서 for문으로 정리 할 수 있음
+    // 2. 범위 설정해서 map, reduce를 사용할 수 있음 
+    // 3. 작은 수 부터 해야 되기 때문에 a 와 b 값을 비교 
+    guard a != b else { return Int64(a) }
+    var resultArray: [Int] = a > b ? (b...a).map{ $0 } : (a...b).map { $0 }
+    let result = resultArray.map { $0 }.reduce(0) { $0 + $1 }
+    
+    return Int64(result)
 }
