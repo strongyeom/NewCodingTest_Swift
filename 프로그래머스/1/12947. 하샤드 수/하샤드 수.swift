c1c39f73c9bf91c87064d2c450603df21c6a2c35
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-09-07 16:23:09 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.swift
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.swift
@@ -1,5 +1,14 @@
 func solution(_ x:Int) -> Bool {
     
-    let sum = String(x).map{ Int(String($0))! }.reduce(0) { $0 + $1 }
-    return x%sum == 0 ? true : false
+    
+    var initX = x
+    var rest = 0
+    while initX != 0 {
+        rest += initX % 10
+        initX = initX / 10
+    }
+    
+    let result = x % rest == 0 ? true : false
+    
+    return result
 }
