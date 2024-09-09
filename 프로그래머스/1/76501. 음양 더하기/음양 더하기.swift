466a9e7e306e2fd2dbfb9ed2bd95d1c82572829f
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-09-09 23:59:37 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
@@ -2,9 +2,15 @@ import Foundation
 
 func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
     
-    var result = 0
+    var result: Int = 0
     for i in 0..<absolutes.count {
-        result += signs[i] == true ? absolutes[i] : -absolutes[i]
+//         if signs[i] == true {
+//             result += absolutes[i]
+//         } else {
+//             result -= absolutes[i]
+//         }
+        
+        result = signs[i] == true ? result + absolutes[i] : result - absolutes[i]
     }
     return result
 }
