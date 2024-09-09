0f23c6fc7f8429de4c5eb72c14be6abcb5dcd787
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-09-10 00:12:18 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 8.82 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.swift
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.swift
@@ -1,8 +1,5 @@
 func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
     
     let result = arr.filter { $0 % divisor == 0 }.sorted(by: <)
-    if result.isEmpty {
-        return [-1]
-    }
-    return result
+    return result.isEmpty ? [-1] : result
 }
