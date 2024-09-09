8a4166789fb34a0070ceb2ec36b8b009747a6a43
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-09-10 00:08:27 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.07 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
@@ -3,7 +3,7 @@ import Foundation
 func solution(_ numbers:[Int]) -> Int {
     
    let total = (0...9).map { $0 }.reduce(0) { $0 + $1 }
-    let value = numbers.map { $0 }.reduce(0) { $0 + $1 }
+    let value = numbers.reduce(0) { $0 + $1 }
     let result = total - value
     return result
 }
