bc962d1d174ba0e1654c89637b5cd3716144d8da
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-09-11 00:00:50 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.08 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
@@ -3,7 +3,8 @@ func solution(_ s:String) -> String {
     let arrayS = s.map { String($0) }
     
     if s.count % 2 == 0 {
-        return arrayS[s.count/2 - 1] + arrayS[s.count/2]
+        // return arrayS[s.count/2 - 1] + arrayS[s.count/2]
+        return Array(arrayS[(s.count/2 - 1)...(s.count/2)]).joined()
     } else {
         return arrayS[s.count/2]
     }
