0bdd105ee7c9b1a05546a8b775210a588e21baef
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-09-10 23:39:46 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.swift
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.swift
@@ -1,19 +1,4 @@
 func solution(_ phone_number:String) -> String {
-    
-    // 뒷 4자리르 제외하고 한번에 바꾸는 방법은 없을까?
-    // 앞에 있는 갯수만큼
-    
-    let count = phone_number.count - 4
-    
-    // 앞에서 부터 해당 번째까지 자르고, * 로 대체
-    // String -> 배열로 변환
-    var arrayConvert = phone_number.map { $0 }
-    
-    var cutArray = arrayConvert[0..<count]
-    var remainArray = arrayConvert[count..<arrayConvert.count].map { String($0) }.joined()
-    let starString = cutArray.map { String($0).replacingOccurrences(of: String($0), with: "*")}.joined()
-    
-    let result = starString + remainArray
-    
-    return result
+    var suffix = phone_number.suffix(4)
+    return String(repeating: "*", count: phone_number.count - 4) + String(suffix)
 }
