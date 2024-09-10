b6a585138d008c772533ba0af2d90c9f9b2f5a93
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-09-10 23:43:39 +0900

    [level 1] Title: 내적, Time: 0.39 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.swift
+++ 프로그래머스/1/70128. 내적/내적.swift
@@ -2,11 +2,6 @@ import Foundation
 
 func solution(_ a:[Int], _ b:[Int]) -> Int {
     
-//     var result: Int = 0
-    
-//     let _ = (0..<a.count).map {
-//         result += a[$0] * b[$0]
-//     }
-    
-    return (0..<a.count).map { a[$0] * b[$0] }.reduce(0) { $0 + $1 }
+    let result = (0..<a.count).map { a[$0] * b[$0] }.reduce(0) { $0 + $1}
+    return result
 }
