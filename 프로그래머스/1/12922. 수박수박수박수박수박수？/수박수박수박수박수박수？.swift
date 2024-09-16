58408c9502dfd3aa6cdb51fdfa4ee14bbee1dea6
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-09-16 17:13:45 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.64 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
@@ -1,15 +1,15 @@
 func solution(_ n:Int) -> String {
-    
     var result: String = ""
-    
-//     for i in 1...n {
-//         result += i%2 == 0 ? "박" : "수"
-//     }
-    
-    
-    let _ = (1...n).map {
-        result += $0%2 == 0 ? "박" : "수"
+    for i in 1...n {
+        if i%2 == 0 {
+            result += "박"
+        } else {
+            result += "수"
+        }
+        // 미리 정해 놓고 n이 짝수 인지 확인 후 몫과 나머지로 하게 되면 더 빠르지 않을까?
     }
+    
+    
     
     return result
 }
