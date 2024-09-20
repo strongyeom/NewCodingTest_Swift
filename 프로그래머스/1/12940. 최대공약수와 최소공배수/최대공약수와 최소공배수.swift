07d86d1841667edf6fad03e3e1a629a7e65d83c2
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-09-20 23:20:08 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.25 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -1,47 +1,44 @@
+import Foundation
+
+func calculater(number: Int) -> Set<Int> {
+    
+    var first = Int(sqrt(Double(number)))
+    var firstSet: Set<Int> = []
+    
+    for i in 1...first {
+        if number % i == 0 {
+            firstSet.insert(i)
+            if i != number / i {
+                firstSet.insert(number/i)
+            }
+        }
+    }
+    
+    return firstSet
+}
+
 func solution(_ n:Int, _ m:Int) -> [Int] {
     
-    // 최소 공배수와 최대 공약수를 구하시오
+    // 최대 공약수와 최소 공배수 
+    // 최대 공약수 두 수를 나누었을때 가장 큰 수
+    // 최소 공배수 두 수의 공통 적인 가장 작은 수 
     
-    // 최소 공배수 : 어떤 수로 나누었을때 떨어지는 값 * 몫
-    // 최대 공약수 : 어떤 수로 나누었을때 떨어지는 가장 큰 값 -> 공통으로 가지는 약수중 가장 큰 수
-    var a1 = Set<Int>()
-    var a2 = Set<Int>()
-    var maxDivideInt: Int = 0
-    var divideValue: [Int] = []
-    // 20, 24
-    for i in 1...n {
-        for j in 1...n {
-            if i*j == n {
-                a1.insert(j)
-            }
-        }
+    // 최대 공약수 두수의 약수중 공통적인 부분중에서 가장 큰 수 
+    
+    // 1. 두수의 약수를 각각 구하고, Set에 넣고 Set의 성질인 교집합 적용 -> Max 값 확인 
+    // 2. 최소 공배수 최대 공약수를 가지고 나누고, 최대 공약수 * 1. 나머지 * 2. 나머지 
+    
+    var firstNumSet = calculater(number: n)
+    var secondNumSet = calculater(number: m)
+    print(firstNumSet, secondNumSet)
+    
+    let interSectionSetMaxNum = firstNumSet.intersection(secondNumSet).max()!
+    print(interSectionSetMaxNum)
+    
+    var minValue = interSectionSetMaxNum * (n / interSectionSetMaxNum) * (m / interSectionSetMaxNum)
+    if interSectionSetMaxNum == 1 {
+       minValue = n * m
     }
-    print(a1)
-    
-    for k in 1...m {
-        for q in 1...m {
-            if k*q == m {
-                a2.insert(q)
-            }
-        }
-    }
-    print(a2)
-    
-    let b1 = a1.intersection(a2).max()!
-    print(b1)
-    
-    maxDivideInt = n > m ? n : m
-    
-    for i in 1...maxDivideInt {
-        var a1 = n/i
-        var b1 = m/i
-        if n%i == 0 && m%i == 0 {
-            divideValue.append(a1 * b1 * i)
-        }
-    }
-    print(divideValue)
-    let minValue = divideValue.min()!
-    print(minValue)
-    
-    return [b1, minValue]
+    print([interSectionSetMaxNum, minValue])
+    return [interSectionSetMaxNum, minValue]
 }
