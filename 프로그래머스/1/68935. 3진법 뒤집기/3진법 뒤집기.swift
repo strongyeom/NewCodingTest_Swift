94494d1cc79056f657935a95a0cadec67f176a73
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-09-22 16:16:24 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.18 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
@@ -1,25 +1,23 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    
-    var value = n
-    var threeValue: [Int] = []
-    var totalValue: Int = 0
-    
-    while value > 0 {
-        var dividedValue = value % 3
-        
-        threeValue.append(dividedValue)
-        value = value / 3
+    // return 0
+    // 3진법 어떻게 하는거야? 
+    // 1200 = * 3^3 + 3^2 * 2 + 1 + 1 
+    // 1 * 3^3 + 2 * 3^2 + 0 * 3 + 0 * 1
+
+    // 1. 3진법으로 해당 수를 만들고  reversed로 쌓이게 한다음 해당 enumulater를 사용해서 인덱스를 구하고 곱하고 더해서 result 도출 
+    var number = n 
+    var list: [Int] = []
+    var result: Int = 0
+    while number > 0 {
+        list.append(number % 3)
+        number = number / 3
     }
     
-    for i in 0..<threeValue.count {
-        var three = pow(3.0, Double(threeValue.count - (i + 1)))
-        totalValue += Int(three) * threeValue[i]
+    // 2. 3진법으로 변환된 값을 뒤집어서 10진법으로 다시 변환
+    for (index, value) in list.reversed().enumerated() {
+        result += value * Int(pow(3.0, Double(index)))
     }
-    
-    // 2진수 -> 10 진수
-    // 1011 = 1 * 2^3 + 0 * 2^2 + 1 * 2^1 + 1 * 2^0
-    
-    return totalValue
+    return result
 }
