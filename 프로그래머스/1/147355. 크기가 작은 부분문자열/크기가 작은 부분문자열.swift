b708b8f5cdfb519f0c7b87019b79d4a856a27878
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-09-22 16:43:54 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 39.67 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.swift
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.swift
@@ -1,19 +1,24 @@
 import Foundation
 
 func solution(_ t:String, _ p:String) -> Int {
+    // 1. 이중 반복문을 상용해서 비교 => 전체를 한번씩 무조건 돌기 때문에 시간복잡도 큼
+    let arrayT = t.map { String($0) }
+    var count: Int = 0
     
-    // 7 , 3 4번의수가 나옴
-    
-    var calCount = t.count - p.count
-    let arrayT: [Character] = t.map { $0 }
-    var arrayResult: [Int] = []
-    for i in 0...calCount {
-        var tCountString: String = ""
-        for j in i..<(p.count + i) {
-            tCountString += String(arrayT[j])
+    for i in 0..<arrayT.count {
+        var firstString: String = String(arrayT[i])
+        for j in i+1..<arrayT.count {
+            if firstString.count != p.count {
+                firstString += arrayT[j]
+            } else {
+                break
+            }
         }
-        arrayResult.append(Int(tCountString)!)
+        if Int(firstString)! <= Int(p)! && firstString.count == p.count {
+            count += 1
+        }
+        
     }
-    print(arrayResult)
-    return arrayResult.filter { $0 <= Int(p)! }.count
+    print(count)
+    return count
 }
