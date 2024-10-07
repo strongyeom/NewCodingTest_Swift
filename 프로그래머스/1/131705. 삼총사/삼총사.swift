241b8f092e2bf88a405b1be77ad1d1e98655a485
Yeom sung pil <101084872+strongyeom@users.noreply.github.com>
2024-10-07 23:09:34 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.swift
+++ 프로그래머스/1/131705. 삼총사/삼총사.swift
@@ -1,19 +1,17 @@
 import Foundation
 
 func solution(_ number:[Int]) -> Int {
-    
-    // 배열에 있는 어떠한 수 3가지를 더해 0이 되면 count + 1 해준다.
-    // 서로 다른 학생의 정수 번호가 같을 수 있음
-    var count = 0
-    for i in 0..<number.count {
-        for j in i+1..<number.count {
-            for k in j+1..<number.count {
-                if number[i] + number[j] + number[k] == 0 {
-                    count += 1
-                   
+    var result = 0
+    for i in 0..<number.count-2 {
+        for j in i+1..<number.count-1 {
+            for z in j+1..<number.count {
+                var count = number[i] + number[j] + number[z]
+                if count == 0 {
+                    result += 1
                 }
             }
         }
     }
-    return count
+    
+    return result
 }
